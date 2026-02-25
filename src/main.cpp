856f1843045ea7b8b3e4a12c49d875bf7ad8779e
gustavo-berti <gustavo.gberti@gmail.com>
2026-02-25 20:56:10 -0300

    feat: adiciona a estrutura de pastas do jogo

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
     GameManager* game = new GameManager();
 
-    if(game->Initialize("Apex Ascent", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false)) {
+    if(game->Initialize("Apex Ascent", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, false)) {
         game->Run();
     }
 
