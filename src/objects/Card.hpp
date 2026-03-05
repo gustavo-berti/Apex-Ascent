9e61bb7541973f0794d401cdbed56e6626c244e1
gustavo-berti <gustavo.gberti@gmail.com>
2026-03-04 21:26:45 -0300

    feat: ajusta carta para superclasse

--- src/objects/Card.hpp
+++ src/objects/Card.hpp
@@ -2,18 +2,23 @@
 #include "./base/DynamicObject.hpp"
 #include <string>
 
-enum class CardType {
-    CREATURE,
-    SPELL
+enum class Rarity {
+    COMMON,
+    UNCOMMON,
+    RARE,
+    EPIC,
+    LEGENDARY
 };
 
 class Card : public DynamicObject {
     private:
         std::string name;
-        CardType type;
+        std::string description;
         int manaCost;
+        Rarity rarity;
+        std::string imagePath;
     public:
-        Card(std::string name, CardType type, int manaCost, int x, int y);
+        Card(std::string name, int manaCost, Rarity rarity, std::string imagePath, int x, int y);
         virtual ~Card();
 
         virtual void Initialize() override;
